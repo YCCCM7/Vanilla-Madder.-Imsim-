3e0542bcb0d3cce6127d05b3f029344526e234df
YCCCM7 <whitechocolatechipclock@gmail.com>
2025-03-25 18:05:48 -0700

    AVMDNihilumCleaner.cpp: -Fixed stupid fucking errors made by nihilum's HUD, and also fixes nihilum main menu function forcing precache. A nice 1-2.

---
+++ VMDNative/Inc/AVMDNihilumCleaner.cpp
@@ -0,0 +1,21 @@
+
+#include "VMDNative.h"
+
+IMPLEMENT_CLASS(AVMDNihilumCleaner);
+
+void AVMDNihilumCleaner::execSilenceNihilum(FFrame &Stack, RESULT_DECL)
+{
+    P_FINISH;
+	
+	//So, we have this stupid function writing to log every frame. Stop it from happening, thanks.
+    UFunction *FuckDrawBorder = Cast<UFunction>(UObject::StaticFindObject(UFunction::StaticClass(), ANY_PACKAGE, TEXT("VMDNative.VMDNihilumCleaner.FuckDrawBorder")));
+    UFunction *NihilumDrawBorder = Cast<UFunction>(UObject::StaticFindObject(UFunction::StaticClass(), ANY_PACKAGE, TEXT("FGRHK.TestHUDHitDisplay.DrawBorder")));
+    NihilumDrawBorder->Script = FuckDrawBorder->Script;
+	
+	//Next, fix the main menu function being chunky as fuck and calling FLUSH.
+    UFunction *NewMainMenu = Cast<UFunction>(UObject::StaticFindObject(UFunction::StaticClass(), ANY_PACKAGE, TEXT("DeusEx.VMDBufferPlayer.VMDNihilumDontFlushMainMenu")));
+    UFunction *NihilumMainMenu = Cast<UFunction>(UObject::StaticFindObject(UFunction::StaticClass(), ANY_PACKAGE, TEXT("FGRHK.MadIngramPlayer.ShowMainMenu")));
+    NihilumMainMenu->Script = NewMainMenu->Script;
+}
+
+IMPLEMENT_FUNCTION(AVMDNihilumCleaner,2208,execSilenceNihilum);
